5f74d19143bb2e599900836c8cf1fb9839e18c97
Nemanja Milovanovic <nemanja@live.nl>
2016-01-16 21:13:58 +0100

    Fix Composition a bit.

--- src/shunting/models/Composition.java
+++ src/shunting/models/Composition.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 
 public class Composition {
 
-
-	ArrayList <Train> trains =new ArrayList<Train> ();
+	private ArrayList<Train> trains = new ArrayList<>();
+	
+	public Composition() {
+		
+	}
 
 	public int size(){
 		return trains.size();
@@ -16,32 +19,33 @@ public class Composition {
 		return trains.get(index);
 	}
 
-	public void replace(int index,Train train){
-
+	public void replace(int index, Train train){
+		
 	}
 
 	public void addTrain(Train train){
-
+		
 	}
 
 	public void addTrain(int index, Train train){
-
+		
 	}
 
 	public void deleteTrain(int index){
-
+		
 	}
 
+	@Override
 	public String toString(){
 		String s = "[";
 		int k=trains.size();
 		for(int i=0;i<k;i++){
-			Train tr= getTrain(i);
-			String id=tr.toString();
-			s=s+id+", ";
+			Train tr = getTrain(i);
+			String id = tr.toString();
+			s += id + ", ";
 		}
+		s = s.substring(0, s.length()-2);
 		s += "]";
 		return s;
-
 	}
 }
