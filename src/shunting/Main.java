d004b899ca246bbe712af102d46806b877dcc0fe
Nemanja Milovanovic <nemanja@live.nl>
2016-01-18 23:34:25 +0100

    Add manual Schedule population.

--- src/shunting/Main.java
+++ src/shunting/Main.java
@@ -8,10 +8,19 @@ import shunting.models.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Arrival a1 = new Arrival(1.0, new Composition());
-		Arrival a2 = new Arrival(2.0, new Composition());
-		Arrival a3 = new Arrival(4.0, new Composition());
-		Arrival a4 = new Arrival(5.0, new Composition());
+		int seed = 0;
+		
+		CompositionFactory cf = new CompositionFactory(seed);		
+		
+		Composition c1 = cf.compDDZ();
+		Composition c2 = cf.compSLT();
+		Composition c3 = cf.compVIRM();
+		Composition c4 = cf.compVIRM();
+		
+		Arrival a1 = new Arrival(1.0, c1);
+		Arrival a2 = new Arrival(2.0, c2);
+		Arrival a3 = new Arrival(4.0, c3);
+		Arrival a4 = new Arrival(5.0, c4);
 		
 		List<Arrival> arrivals = new ArrayList<>();
 		arrivals.add(a1); arrivals.add(a2);
