bdf7d6f852f6a615a4a59a063e25bbac02b9d538
Nemanja Milovanovic <nemanja@live.nl>
2016-01-19 00:05:49 +0100

    Add graph/network extraction feature to Composition.

--- src/shunting/models/Composition.java
+++ src/shunting/models/Composition.java
@@ -1,13 +1,21 @@
 package shunting.models;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.graph.DefaultDirectedGraph;
 
 public class Composition {
 
-	private ArrayList<Train> trains = new ArrayList<>();
+	private List<Train> trains;
 	
 	public Composition() {
-		
+		trains = new ArrayList<>();
+	}
+	
+	public Composition(List<Train> trains) {
+		this.trains = trains;
 	}
 
 	public int size(){
@@ -34,6 +42,48 @@ public class Composition {
 	public void deleteTrain(int index){
 		trains.remove(index);
 	}
+	
+	public DirectedGraph<Train, Part> getGraph() {
+		DirectedGraph<Train, Part> dgraph = new DefaultDirectedGraph<>(Part.class);
+		
+		// add nodes
+		// dummy
+		Train dummy = Train.dummy();
+		dgraph.addVertex(dummy);
+		int n = size();
+		for (int i = 0; i < n; i++) {
+			Train v = trains.get(i);
+			Part p = getPart(0,i+1);
+			dgraph.addVertex(v);
+			dgraph.addEdge(dummy, v, p);
+		}
+		
+		// add edges
+		for (int i = 0; i < n-1; i++) {
+			for (int j = i+1; j < n; j++) {
+				Train u = trains.get(i);
+				Train v = trains.get(j);
+				Part p = getPart(i+1, j+1);
+				dgraph.addEdge(u, v, p);
+			}
+		}
+		
+		return dgraph;
+	}
+	
+	/**
+	 * Return the part represented by (i,j) in the network for the Train Unit Matching Problem, where index i is inclusive and j is exclusive.
+	 * 
+	 * @param i First train unit, inclusive
+	 * @param j Last train unit, inclusive
+	 * @return
+	 */
+	private Part getPart(int i, int j) {
+		Part p = new Part();
+		for (int k = i; k < j; k++)
+			p.addUnit(trains.get(k));
+		return p;
+	}
 
 	@Override
 	public String toString(){
@@ -41,9 +91,10 @@ public class Composition {
 		int k=trains.size();
 		for(int i=0;i<k;i++){
 			Train tr = getTrain(i);
-			String id = tr.toString();
+			String id = tr.getID();
+			s += id;
 			if (i < k-1)
-				s += id + ", ";
+				s += ", ";
 		}
 		s += "]";
 		return s;
