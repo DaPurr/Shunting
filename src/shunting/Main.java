bb0c7138f9367e2f071f50eef62ca48d69fa332b
Nemanja Milovanovic <nemanja@live.nl>
2016-01-19 00:06:20 +0100

    Add test cases for networks used in matching problem.

--- src/shunting/Main.java
+++ src/shunting/Main.java
@@ -3,13 +3,15 @@ package shunting;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jgrapht.DirectedGraph;
+
 import shunting.models.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		int seed = 0;
-		
+
 		CompositionFactory cf = new CompositionFactory(seed);		
 		
 		Composition c1 = cf.compDDZ();
@@ -17,6 +19,16 @@ public class Main {
 		Composition c3 = cf.compVIRM();
 		Composition c4 = cf.compVIRM();
 		
+		DirectedGraph<Train, Part> dgraph1 = c1.getGraph();
+		DirectedGraph<Train, Part> dgraph2 = c2.getGraph();
+		DirectedGraph<Train, Part> dgraph3 = c3.getGraph();
+		DirectedGraph<Train, Part> dgraph4 = c4.getGraph();
+		
+		System.out.println(dgraph1.toString());
+		System.out.println(dgraph2.toString());
+		System.out.println(dgraph3.toString());
+		System.out.println(dgraph4.toString());
+
 		Arrival a1 = new Arrival(1.0, c1);
 		Arrival a2 = new Arrival(2.0, c2);
 		Arrival a3 = new Arrival(4.0, c3);
