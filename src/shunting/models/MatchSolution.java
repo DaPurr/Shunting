aacbc0ab94d20cbbbca77def9e30d021a2147239
Nemanja Milovanovic <nemanja@live.nl>
2016-01-19 11:06:33 +0100

    Create MatchSolution the real solution to Matching Problem.

---
+++ src/shunting/models/MatchSolution.java
@@ -0,0 +1,21 @@
+package shunting.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MatchSolution {
+
+	private Set<MatchBlock> solution;
+	
+	public MatchSolution() {
+		solution = new HashSet<>();
+	}
+	
+	public void addBlock(MatchBlock block) {
+		solution.add(block);
+	}
+	
+	public boolean contains(MatchBlock block) {
+		return solution.contains(block);
+	}
+}
