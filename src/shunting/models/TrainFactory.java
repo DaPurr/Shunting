e1eac88892ddbc356ae2a55fe1209e0e82b16261
Nemanja Milovanovic <nemanja@live.nl>
2016-01-19 13:54:27 +0100

    Add several wrapper functions to ease train creation.

--- src/shunting/models/TrainFactory.java
+++ src/shunting/models/TrainFactory.java
@@ -3,53 +3,118 @@ package shunting.models;
 import java.util.Random;
 
 public class TrainFactory {
-	
+
 	private Random ran;
+	private final TrainType VIRM4 = new TrainType("VIRM_4", 109, 0, 0, 0, 0);
+	private final TrainType VIRM6 = new TrainType("VIRM_6", 162, 0, 0, 0, 0);
+	private final TrainType DDZ4 = new TrainType("DDZ_4", 101, 0, 0, 0, 0);
+	private final TrainType DDZ6 = new TrainType("DDZ_6", 154, 0, 0, 0, 0);
+	private final TrainType SLT4 = new TrainType("SLT_4", 70, 0, 0, 0, 0);
+	private final TrainType SLT6 = new TrainType("SLT_6", 101, 0, 0, 0, 0);
+
+	public TrainFactory() {
+		ran = new Random(0);
+	}
 
 	public TrainFactory(int seed) {
 		ran = new Random(seed);
 	}
-	
+
 	public TrainFactory(Random ran) {
 		this.ran = ran;
 	}
-	
+
+	public Train createTrainByType(String ID, String type, boolean interchangeable, boolean inspection, 
+			boolean repair, boolean cleaning, boolean washing) {
+		String[] parts = type.split("_");
+		type = parts[0];
+		int subtype = Integer.parseInt(parts[1]);
+		if (type.equals("VIRM")) {
+			if (subtype == 4)
+				return typeVIRM4(ID, interchangeable, inspection, repair, cleaning, washing);
+			else if (subtype == 6)
+				return typeVIRM6(ID, interchangeable, inspection, repair, cleaning, washing);
+		} else if (type.equals("DDZ")) {
+			if (subtype == 4)
+				return typeDDZ4(ID, interchangeable, inspection, repair, cleaning, washing);
+			else if (subtype == 6)
+				return typeDDZ6(ID, interchangeable, inspection, repair, cleaning, washing);
+		} else if (type.equals("SLT")) {
+			if (subtype == 4)
+				return typeSLT4(ID, interchangeable, inspection, repair, cleaning, washing);
+			else if (subtype == 6)
+				return typeSLT6(ID, interchangeable, inspection, repair, cleaning, washing);
+		}
+		
+		throw new IllegalArgumentException("Rolling stock type " + type +" not known.");
+	}
+
+	public Train typeVIRM4(String ID, boolean interchangeable, boolean inspection, 
+			boolean repair, boolean cleaning, boolean washing) {
+		return new Train(ID, interchangeable, inspection, 
+				repair, cleaning, washing, VIRM4);
+	}
+
 	public Train typeVIRM4() {
-		TrainType type = new TrainType("VIRM_4", 109, 0, 0, 0, 0);
 		return new Train(generateID(), ran.nextBoolean(), ran.nextBoolean(), 
-				ran.nextBoolean(), ran.nextBoolean(), ran.nextBoolean(), type);
+				ran.nextBoolean(), ran.nextBoolean(), ran.nextBoolean(), VIRM4);
 	}
-	
+
+	public Train typeVIRM6(String ID, boolean interchangeable, boolean inspection, 
+			boolean repair, boolean cleaning, boolean washing) {
+		return new Train(ID, interchangeable, inspection, 
+				repair, cleaning, washing, VIRM6);
+	}
+
 	public Train typeVIRM6() {
-		TrainType type = new TrainType("VIRM_6", 162, 0, 0, 0, 0);
 		return new Train(generateID(), ran.nextBoolean(), ran.nextBoolean(), 
-				ran.nextBoolean(), ran.nextBoolean(), ran.nextBoolean(), type);
+				ran.nextBoolean(), ran.nextBoolean(), ran.nextBoolean(), VIRM6);
 	}
-	
+
+	public Train typeDDZ4(String ID, boolean interchangeable, boolean inspection, 
+			boolean repair, boolean cleaning, boolean washing) {
+		return new Train(ID, interchangeable, inspection, 
+				repair, cleaning, washing, DDZ4);
+	}
+
 	public Train typeDDZ4() {
-		TrainType type = new TrainType("DDZ_4", 101, 0, 0, 0, 0);
 		return new Train(generateID(), ran.nextBoolean(), ran.nextBoolean(), 
-				ran.nextBoolean(), ran.nextBoolean(), ran.nextBoolean(), type);
+				ran.nextBoolean(), ran.nextBoolean(), ran.nextBoolean(), DDZ4);
 	}
-	
+
+	public Train typeDDZ6(String ID, boolean interchangeable, boolean inspection, 
+			boolean repair, boolean cleaning, boolean washing) {
+		return new Train(ID, interchangeable, inspection, 
+				repair, cleaning, washing, DDZ6);
+	}
+
 	public Train typeDDZ6() {
-		TrainType type = new TrainType("DDZ_6", 154, 0, 0, 0, 0);
 		return new Train(generateID(), ran.nextBoolean(), ran.nextBoolean(), 
-				ran.nextBoolean(), ran.nextBoolean(), ran.nextBoolean(), type);
+				ran.nextBoolean(), ran.nextBoolean(), ran.nextBoolean(), DDZ6);
 	}
-	
+
+	public Train typeSLT4(String ID, boolean interchangeable, boolean inspection, 
+			boolean repair, boolean cleaning, boolean washing) {
+		return new Train(ID, interchangeable, inspection, 
+				repair, cleaning, washing, SLT4);
+	}
+
 	public Train typeSLT4() {
-		TrainType type = new TrainType("SLT_4", 70, 0, 0, 0, 0);
 		return new Train(generateID(), ran.nextBoolean(), ran.nextBoolean(), 
-				ran.nextBoolean(), ran.nextBoolean(), ran.nextBoolean(), type);
+				ran.nextBoolean(), ran.nextBoolean(), ran.nextBoolean(), SLT4);
 	}
-	
+
+	public Train typeSLT6(String ID, boolean interchangeable, boolean inspection, 
+			boolean repair, boolean cleaning, boolean washing) {
+		return new Train(ID, interchangeable, inspection, 
+				repair, cleaning, washing, SLT6);
+	}
+
 	public Train typeSLT6() {
-		TrainType type = new TrainType("SLT_6", 101, 0, 0, 0, 0);
 		return new Train(generateID(), ran.nextBoolean(), ran.nextBoolean(), 
-				ran.nextBoolean(), ran.nextBoolean(), ran.nextBoolean(), type);
+				ran.nextBoolean(), ran.nextBoolean(), ran.nextBoolean(), SLT6);
 	}
-	
+
 	private String generateID() {
 		return String.valueOf(1 + ran.nextInt(10000));
 	}
