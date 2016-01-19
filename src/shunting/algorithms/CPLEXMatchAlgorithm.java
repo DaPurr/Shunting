c8596bec85dc742fba4211dd93267509411c07fb
Nemanja Milovanovic <nemanja@live.nl>
2016-01-19 10:55:49 +0100

    Create class CPLEXMatchAlgorithm solving the Matching Problem by basic math formulation.

---
+++ src/shunting/algorithms/CPLEXMatchAlgorithm.java
@@ -0,0 +1,13 @@
+package shunting.algorithms;
+
+import shunting.models.MatchBlock;
+
+public class CPLEXMatchAlgorithm implements MatchAlgorithm {
+
+	@Override
+	public MatchBlock solve() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
