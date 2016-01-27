7b69e5b874d860afa6f5be423afeeecd452d5650
Nemanja Milovanovic <nemanja@live.nl>
2016-01-27 12:05:42 +0100

    Create (incomplete) skeleton for shunting yard.

---
+++ src/shunting/models/ShuntingYard.java
@@ -0,0 +1,21 @@
+package shunting.models;
+
+import java.util.List;
+
+public class ShuntingYard {
+
+	private List<Platform> platforms;
+	private List<Washer> washers;
+	
+	public ShuntingYard() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public List<Platform> getPlatforms() {
+		return platforms;
+	}
+	
+	public List<Washer> getWashers() {
+		return washers;
+	}
+}
