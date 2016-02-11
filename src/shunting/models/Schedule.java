13586bf746e29508cb3ead801a43f1e513fb0820
Faisal Shaban <Faisal Shaban>
2016-02-11 13:02:51 +0100

    random schedule function finished, still needs testing/debugging

--- src/shunting/models/Schedule.java
+++ src/shunting/models/Schedule.java
@@ -52,7 +52,7 @@ public class Schedule {
 		return s;
 	}
 
-	public static void randomSchedule(int nrTrainUnits, int horizon, Random rn){ //change void to schedule later
+	public static Schedule randomSchedule(int nrTrainUnits, int horizon, Random rn){ 
 
 		List<Arrival> arrivals = new ArrayList<>();
 		int unitsNow=0;
@@ -66,28 +66,196 @@ public class Schedule {
 			Composition ddz=p.compDDZ();
 			Composition slt=p.compSLT();
 
-			int answer = rn.nextInt(4) + 1;
+			int answer = rn.nextInt(3) + 1;
 
 			if (answer==1){
 				unitsNow=unitsNow+virm.size();
-			Arrival a=new Arrival(arrivalTime,virm);
-			arrivals.add(a);}
+				Arrival a=new Arrival(arrivalTime,virm);
+				arrivals.add(a);}
 
 			if (answer==2){
 				unitsNow=unitsNow+ddz.size();
-			Arrival b=new Arrival(arrivalTime,ddz);
-			arrivals.add(b);}
+				Arrival b=new Arrival(arrivalTime,ddz);
+				arrivals.add(b);}
 
 			if (answer==3){
 				unitsNow=unitsNow+slt.size();
-			Arrival c=new Arrival(arrivalTime,ddz);
-			arrivals.add(c);}
+				Arrival c=new Arrival(arrivalTime,slt);
+				arrivals.add(c);}
+
+		}
+
+		List<Departure> departureVirm = new ArrayList<>();
+		List<Departure> departureDdz = new ArrayList<>();
+		List<Departure> departureSlt = new ArrayList<>();
+
+		List<Train> listVirm = new ArrayList<>();
+		List<Train> listDdz = new ArrayList<>();
+		List<Train> listSlt = new ArrayList<>();
+
+		for(Arrival i:arrivals){
+			Composition comp=i.getComposition();
+
+			for(int k=0;k<comp.size();k++){
+
+				Train arrtrain=comp.getTrain(k);
+				TrainType type=arrtrain.getTrainType();
+
+				if(type.toString()=="VIRM_4" || type.toString()=="VIRM_6"){
+					listVirm.add(arrtrain);
+				}
+				if(type.toString()=="DDZ_4" || type.toString()=="DDZ_6"){
+					listDdz.add(arrtrain);
+				}
+				if(type.toString()=="SLT_4" || type.toString()=="SLT_6"){
+					listSlt.add(arrtrain);
+				}
+
+			}	
+		}
+
+		while(listVirm.isEmpty()==false){
+			int compSize = rn.nextInt(3) + 1;
+			int departureTime=rn.nextInt(horizon+1)+horizon/2;
+
+			if(compSize==1){
+				Train depTrain=listVirm.get(0);
+				List <Train> trainComp= new ArrayList<>();
+				trainComp.add(depTrain);
+				Composition comp1=new Composition("id",trainComp);
+				Departure depVIRM=new Departure(departureTime,comp1);
+				departureVirm.add(depVIRM);
+				listVirm.remove(0);
+			}
+			if(compSize==2){
+				Train depTrain1=listVirm.get(0);
+				Train depTrain2=listVirm.get(1);
+				List <Train> trainComp= new ArrayList<>();
+				trainComp.add(depTrain1);
+				if(listVirm.get(1)!=null){trainComp.add(depTrain2);}
+				Composition comp1=new Composition("id",trainComp);
+				Departure depVIRM=new Departure(departureTime,comp1);
+				departureVirm.add(depVIRM);
+				listVirm.remove(0);
+				if(listVirm.get(1)!=null){listVirm.remove(1);}
+			}
+			if(compSize==3){
+				Train depTrain1=listVirm.get(0);
+				Train depTrain2=listVirm.get(1);
+				Train depTrain3=listVirm.get(2);
+				List <Train> trainComp= new ArrayList<>();
+				trainComp.add(depTrain1);
+				if(listVirm.get(1)!=null){trainComp.add(depTrain2);}
+				if(listVirm.get(2)!=null){trainComp.add(depTrain3);}
+				Composition comp1=new Composition("id",trainComp);
+				Departure depVIRM=new Departure(departureTime,comp1);
+				departureVirm.add(depVIRM);
+				listVirm.remove(0);
+				if(listVirm.get(1)!=null){listVirm.remove(1);}
+				if(listVirm.get(2)!=null){listVirm.remove(2);}
+			}
 
 		}
 		
-		Collections.sort(arrivals);
+		while(listDdz.isEmpty()==false){
+			int compSize = rn.nextInt(3) + 1;
+			int departureTime=rn.nextInt(horizon+1)+horizon/2;
+
+			if(compSize==1){
+				Train depTrain=listDdz.get(0);
+				List <Train> trainComp= new ArrayList<>();
+				trainComp.add(depTrain);
+				Composition comp1=new Composition("id",trainComp);
+				Departure depDDZ=new Departure(departureTime,comp1);
+				departureDdz.add(depDDZ);
+				listDdz.remove(0);
+			}
+			if(compSize==2){
+				Train depTrain1=listDdz.get(0);
+				Train depTrain2=listDdz.get(1);
+				List <Train> trainComp= new ArrayList<>();
+				trainComp.add(depTrain1);
+				if(listDdz.get(1)!=null){trainComp.add(depTrain2);}
+				Composition comp1=new Composition("id",trainComp);
+				Departure depDdz=new Departure(departureTime,comp1);
+				departureDdz.add(depDdz);
+				listDdz.remove(0);
+				if(listDdz.get(1)!=null){listDdz.remove(1);}
+			}
+			if(compSize==3){
+				Train depTrain1=listDdz.get(0);
+				Train depTrain2=listDdz.get(1);
+				Train depTrain3=listDdz.get(2);
+				List <Train> trainComp= new ArrayList<>();
+				trainComp.add(depTrain1);
+				if(listDdz.get(1)!=null){trainComp.add(depTrain2);}
+				if(listDdz.get(2)!=null){trainComp.add(depTrain3);}
+				Composition comp1=new Composition("id",trainComp);
+				Departure depDdz=new Departure(departureTime,comp1);
+				departureDdz.add(depDdz);
+				listDdz.remove(0);
+				if(listDdz.get(1)!=null){listDdz.remove(1);}
+				if(listDdz.get(2)!=null){listDdz.remove(2);}
+			}
+
+		}
 		
+		while(listSlt.isEmpty()==false){
+			int compSize = rn.nextInt(3) + 1;
+			int departureTime=rn.nextInt(horizon+1)+horizon/2;
+
+			if(compSize==1){
+				Train depTrain=listSlt.get(0);
+				List <Train> trainComp= new ArrayList<>();
+				trainComp.add(depTrain);
+				Composition comp1=new Composition("id",trainComp);
+				Departure depSlt=new Departure(departureTime,comp1);
+				departureSlt.add(depSlt);
+				listSlt.remove(0);
+			}
+			if(compSize==2){
+				Train depTrain1=listSlt.get(0);
+				Train depTrain2=listSlt.get(1);
+				List <Train> trainComp= new ArrayList<>();
+				trainComp.add(depTrain1);
+				if(listSlt.get(1)!=null){trainComp.add(depTrain2);}
+				Composition comp1=new Composition("id",trainComp);
+				Departure depSlt=new Departure(departureTime,comp1);
+				departureSlt.add(depSlt);
+				listSlt.remove(0);
+				if(listSlt.get(1)!=null){listSlt.remove(1);}
+			}
+			if(compSize==3){
+				Train depTrain1=listSlt.get(0);
+				Train depTrain2=listSlt.get(1);
+				Train depTrain3=listSlt.get(2);
+				List <Train> trainComp= new ArrayList<>();
+				trainComp.add(depTrain1);
+				if(listSlt.get(1)!=null){trainComp.add(depTrain2);}
+				if(listSlt.get(2)!=null){trainComp.add(depTrain3);}
+				Composition comp1=new Composition("id",trainComp);
+				Departure depSlt=new Departure(departureTime,comp1);
+				departureSlt.add(depSlt);
+				listSlt.remove(0);
+				if(listSlt.get(1)!=null){listSlt.remove(1);}
+				if(listSlt.get(2)!=null){listSlt.remove(2);}
+			}
+
+
+		}
+		List<Departure> departure = new ArrayList<Departure>();
+		departure.addAll(departureVirm);
+		departure.addAll(departureDdz);
+		departure.addAll(departureSlt);
+		
+		Collections.sort(arrivals);
+		Collections.sort(departure);
+		
+		Schedule randomSchedule=new Schedule(arrivals,departure);
+		
+		return randomSchedule;
 		
 	}
+	
 
 }
