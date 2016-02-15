3c2d5dc0997dda8b311debac8baf23233dcee60f
Nemanja Milovanovic <nemanja@live.nl>
2016-02-15 01:03:19 +0100

    Add debugging/testing funtionality for column generation and related classes.

--- src/shunting/algorithms/CGParkingAlgorithm.java
+++ src/shunting/algorithms/CGParkingAlgorithm.java
@@ -9,7 +9,7 @@ import ilog.concert.*;
 
 public class CGParkingAlgorithm implements ParkingAlgorithm {
 
-	private final int D_PARK = 100;
+	private final int D_PARK = 10000; // penalty for not parking a block
 	
 	private Set<MatchBlock> matches;
 	private Set<ShuntTrack> tracks;
@@ -19,7 +19,7 @@ public class CGParkingAlgorithm implements ParkingAlgorithm {
 	private Map<ShuntTrack, IloRange> capacityConstraints = new HashMap<>();
 	
 	private Map<MatchBlock, IloNumVar> notParked = new HashMap<>();
-	private Map<TrackAssignment, IloNumVar> assignment = new HashMap<>();
+	private Map<Path, IloNumVar> assignment = new HashMap<>();
 	private IloObjective objective;
 	
 	public CGParkingAlgorithm(Set<MatchBlock> matches, ShuntingYard yard) throws IloException {
@@ -50,11 +50,41 @@ public class CGParkingAlgorithm implements ParkingAlgorithm {
 		
 		// create pricing problem
 		PricingProblem pricingProblem = new PricingProblem(tracks, matches);
+		Path path = null;
 		
 		// perform column generation
 		while (true) {
 			master.solve();
-			break;
+			
+			// display solution
+			displayVariables();
+			
+			System.out.println("DUALS:");
+			System.out.println("------------------------------------");
+			// update lambda's
+			for (MatchBlock mb : coverageConstraints.keySet()) {
+				IloRange constraint = coverageConstraints.get(mb);
+				double dual = master.getDual(constraint);
+				pricingProblem.setDualLambda(mb, dual);
+				System.out.println("Coverage\t" + mb.toString() + ": " + dual);
+			}
+			
+			// update mu's
+			for (ShuntTrack track : capacityConstraints.keySet()) {
+				IloRange constraint = capacityConstraints.get(track);
+				double dual = master.getDual(constraint);
+				pricingProblem.setDualMu(track, dual);
+				System.out.println("Capacity\t" + track.toString() + ": " + dual);
+			}
+			System.out.println("------------------------------------");
+			
+			path = pricingProblem.solve();
+			if (path == null)
+				break;
+			
+			// add generated column
+			IloNumVar genCol = addAssignmentVariable(path, path.getPathCost());
+			assignment.put(path, genCol);
 		}
 	}
 
@@ -71,9 +101,9 @@ public class CGParkingAlgorithm implements ParkingAlgorithm {
 		notParked.put(match, isParked);
 	}
 	
-	private IloNumVar addAssignmentVariable(TrackAssignment ass, double cost) throws IloException {
+	private IloNumVar addAssignmentVariable(Path ass, double cost) throws IloException {
 		IloColumn column = master.column(objective, cost);
-		for (MatchBlock match : matches) {
+		for (MatchBlock match : ass.coveredBlocks()) {
 			IloRange constraint = coverageConstraints.get(match);
 			column = column.and(master.column(constraint, 1));
 		}
@@ -101,6 +131,20 @@ public class CGParkingAlgorithm implements ParkingAlgorithm {
 		for (ShuntTrack track : tracks) {
 			capacityConstraints.put(track, master.addRange(0, 1));
 		}
+	}
+	
+	private void displayVariables() throws IloException {
+		System.out.println("SOLUTION:");
+		System.out.println("------------------------------------");
+		for (MatchBlock block : notParked.keySet()) {
+			IloNumVar var = notParked.get(block);
+			System.out.println("N_b\t" + block.toString() + ": " + master.getValue(var));
+		}
+		for (Path path : assignment.keySet()) {
+			IloNumVar var = assignment.get(path);
+			System.out.println("X_a^s\t" + path.toString() + ": " + master.getValue(var));
+		}
+		System.out.println("------------------------------------");
 	}
 
 }
--- src/shunting/test/LIFOPathTest.java
+++ src/shunting/test/LIFOPathTest.java
@@ -1,9 +1,55 @@
 package shunting.test;
 
-public class LIFOPathTest {
+import java.util.*;
 
-	public static void main(String[] args) {
+import ilog.concert.IloException;
+import shunting.algorithms.*;
+import shunting.models.*;
+
+public class LIFOPathTest {
+	
+	private final static int horizon = 1600;
+
+	public static void main(String[] args) {		
+		ShuntTrack track = new LIFOShuntTrack(1000);
+		List<ShuntTrack> tracks = new ArrayList<>();
+		tracks.add(track);
+		TrainFactory tf = new TrainFactory();
+		Train t1 = tf.typeDDZ4("1", true, false, false, false, false);
+		Train t2 = tf.typeDDZ4("2", true, false, false, false, false);
+		Train t3 = tf.typeDDZ4("3", true, false, false, false, false);
+		Train t4 = tf.typeVIRM4("5", true, false, false, false, false);
+		Train t5 = tf.typeSLT4("6", true, false, false, false, false);
+		Part p1 = new Part(); p1.addUnit(t1);
+		Part p2 = new Part(); p2.addUnit(t4);
+		Part p3 = new Part(); p3.addUnit(t5);
+		Part p4 = new Part(); p4.addUnit(t2);
+		Part p5 = new Part(); p5.addUnit(t3);
+		MatchBlock mb1 = new MatchBlock(p1, p1, 0, 666, 2, 3);
+		MatchBlock mb2 = new MatchBlock(p2, p2, 83, 544, 0, 3);
+		MatchBlock mb3 = new MatchBlock(p3, p3, 146, 635, 2, 0);
+		MatchBlock mb4 = new MatchBlock(p4, p4, 152, 603, 2, 3);
+		Set<MatchBlock> matches = new HashSet<>();
+		matches.add(mb1); matches.add(mb2); 
+		matches.add(mb3); matches.add(mb4);
 		
+		List<Platform> platforms = new ArrayList<Platform>();
+		List<Washer> washers = new ArrayList<Washer>();
+		Platform platform1 = new Platform(horizon);
+		Platform platform2 = new Platform(horizon);
+		Washer washer1 = new Washer(horizon);
+		platforms.add(platform1);
+		platforms.add(platform2);
+		washers.add(washer1);
+		
+		ShuntingYard shuntingYard = new ShuntingYard(platforms, washers, tracks);
+		
+		try {
+			ParkingAlgorithm cg = new CGParkingAlgorithm(matches, shuntingYard);
+			cg.solve();
+		} catch (IloException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
