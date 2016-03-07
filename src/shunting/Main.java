e99436efd4e908b9e8b851a65296d8bc34fbf035
Tatiana <gorgiledzhan@gmail.com>
2016-03-07 09:37:50 +0100

    Changes in main

--- src/shunting/Main.java
+++ src/shunting/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		int horizon = 1440;
-		int maxNrTrainUnits = 100;
+		int maxNrTrainUnits = 101;
 		int numberOfSeeds = 100;
 
 
@@ -28,12 +28,18 @@ public class Main {
 
 
 
-		/*for (int trains=30; trains<maxNrTrainUnits; trains=trains + 5){
-			//System.out.println("number of trains is:" + trains);
+
+		List<Double> averageNumberNeedInspection = new ArrayList<>();
+		List<Double> averageNumberNeedCleaning = new ArrayList<>();
+		List<Double> averageNumberNeedWashing = new ArrayList<>();
+		List<Double> averageNumberNeedRepair = new ArrayList<>();
+		/*
+		for (int trains=29; trains<maxNrTrainUnits; trains=trains+1){
+			System.out.println("number of trains is:" + trains);
 			int countMatch = 0;
 			nrTrains.add(trains);
 
-				for (int seed=0; seed<numberOfSeeds; seed++){
+			for (int seed=0; seed<numberOfSeeds; seed++){
 
 				Random rn = new Random(seed);
 				Schedule test = Schedule.randomSchedule(trains, horizon, rn);
@@ -79,24 +85,33 @@ public class Main {
 			double avgrepair=sumrepair/numberOfSeeds;
 			double avginspect=suminspect/numberOfSeeds;
 
-			System.out.println("Average number of trains that need cleaning: "+avgcleaning+ ", washing: "+avgwashing+ ", repair: "+avgrepair+", inspection: "+avginspect);
+			averageNumberNeedCleaning.add(avgcleaning);
+			averageNumberNeedInspection.add(avginspect);
+			averageNumberNeedWashing.add(avgwashing);
+			averageNumberNeedRepair.add(avgrepair);
+
+			//System.out.println("Average number of trains that need cleaning: "+avgcleaning+ ", washing: "+avgwashing+ ", repair: "+avgrepair+", inspection: "+avginspect);
 
 			int count = 0;
 			for(Boolean temp : feasible){
 				if(temp){ count++;}
 			}
 			double frac = (double) (count+countMatch)/(feasible.size());
-			System.out.println("Fraction of feasible solutions using different booleans: " + frac);
+			//System.out.println("Fraction of feasible solutions using different booleans: " + frac);
 			fractions.add(frac);
 		}
 		for (int i=0;i<fractions.size();i++){
 			System.out.println("With "+ nrTrains.get(i) + " trains the fraction of feasible solutions is "+ fractions.get(i));
 		}
 
+		System.out.println("Average number need inspection "+averageNumberNeedInspection.toString());
+		System.out.println("Average number need cleaning "+ averageNumberNeedCleaning.toString());
+		System.out.println("Average number need washing "+averageNumberNeedWashing.toString());
+		System.out.println("Average number need repair "+averageNumberNeedRepair.toString());
+	*/
 
-	}
-			 */
 
+		/*
 
 			File file = new File("data/schedule_kleine_binckhorst_real_nomark.xml");
 			int sumcleaning=0;
@@ -105,21 +120,21 @@ public class Main {
 			int suminspect=0;
 			int countMatch = 0;
 			double totalRunningTime = 0;
-			
+
 			for (int seed=0; seed<numberOfSeeds; seed++){
 				long startTime = System.nanoTime();
 				ScheduleReader sr = new ScheduleReader(seed);
 				Schedule schedule = sr.parseXML(file);
 				schedules.add(schedule);
 				Initialisation_procedure initialisation = new Initialisation_procedure();
-				
+
 
 				ShuntingYard kb =  initialisation.initialisation(horizon);
 				Procedure proc = new Procedure(schedule, kb, horizon);
 				Boolean procedureFeasible = proc.solve();
 				feasible.add(procedureFeasible);
 				countMatch = countMatch + proc.getCounterMatching();
-				
+
 
 				int cleaning = countCleaning(schedule);
 				countCleaning.add(cleaning);
@@ -132,9 +147,9 @@ public class Main {
 				numberOfReruns.add(proc.numberOfReruns);
 				double runningTime = (System.nanoTime()-startTime)*Math.pow(10, 9);
 				runningTimes.add(runningTime);
-				
-				
-				
+
+
+
 
 				//System.out.println("Number of trains that need cleaning is " + cleaning);
 				//System.out.println("Number of trains that need washing is " + washing);
@@ -148,7 +163,7 @@ public class Main {
 			int minInspection = Integer.MAX_VALUE;
 			int minNumberOfReruns = Integer.MAX_VALUE;
 			double minRunningTime = Double.MAX_VALUE;
-			
+
 			int maxCleaning = 0;
 			int maxWashing = 0;
 			int maxRepair = 0;
@@ -156,7 +171,7 @@ public class Main {
 			int maxNumberOfReruns = 0;
 			int sumNumberOfReruns = 0;
 			double maxRunningTime = 0;
-			
+
 			for (int i=0; i< numberOfSeeds;i++) {
 				sumcleaning = sumcleaning+countCleaning.get(i);
 				sumwashing = sumwashing+countWashing.get(i);
@@ -164,26 +179,26 @@ public class Main {
 				suminspect = suminspect+countInspection.get(i);
 				sumNumberOfReruns = sumNumberOfReruns+numberOfReruns.get(i);
 				totalRunningTime = totalRunningTime+runningTimes.get(i);
-				
-				
+
+
 				if(minCleaning>countCleaning.get(i)) { minCleaning = countCleaning.get(i);}
 				if(minWashing>countWashing.get(i)) { minWashing = countWashing.get(i);}
 				if(minRepair>countRepair.get(i)) { minRepair = countRepair.get(i);}
 				if(minInspection>countInspection.get(i)) { minInspection = countInspection.get(i);}
 				if(minNumberOfReruns>numberOfReruns.get(i)) {minNumberOfReruns = numberOfReruns.get(i);}
 				if(minRunningTime>runningTimes.get(i)) {minRunningTime = runningTimes.get(i);}
-				
+
 				if(maxCleaning<countCleaning.get(i)) { maxCleaning = countCleaning.get(i);}
 				if(maxWashing<countWashing.get(i)) { maxWashing = countWashing.get(i);}
 				if(maxRepair<countRepair.get(i)) { maxRepair = countRepair.get(i);}
 				if(maxInspection<countInspection.get(i)) { maxInspection = countInspection.get(i);}
 				if(maxNumberOfReruns<numberOfReruns.get(i)) { maxNumberOfReruns = numberOfReruns.get(i);}
 				if(maxRunningTime<runningTimes.get(i)) { maxRunningTime = runningTimes.get(i);}
-				
-				
+
+
 			}
-			
-			
+
+
 
 			double avgcleaning=sumcleaning/numberOfSeeds;
 			double avgwashing=sumwashing/numberOfSeeds;
@@ -207,9 +222,57 @@ public class Main {
 			double frac = (double) count/(feasible.size()-countMatch);
 			System.out.println("Fraction of feasible solutions using different booleans: " + frac);
 			System.out.println(countMatch);
-			
-		
+		 */
+
+		//simple check IMPORTANT: it does not lead to feasible/non-feasible outcome
+	
+		for (int trains=98; trains<maxNrTrainUnits; trains=trains+1){
+			System.out.println("number of trains is:" + trains);
+			int sumcleaning =0;
+			int sumrepair = 0;
+			int suminspect = 0;
+			int sumwashing = 0;
+			for (int seed=0; seed<numberOfSeeds; seed++){
+
+				Random rn = new Random(seed);
+				Schedule test = Schedule.randomSchedule(trains, horizon, rn);
+				Schedule schedule=test;
+				schedules.add(schedule);
+
+				int cleaning = countCleaning(schedule);
+				countCleaning.add(cleaning);
+				int washing = countWashing(schedule);
+				countWashing.add(washing);
+				int repair = countRepair(schedule);
+				countRepair.add(repair);
+				int inspection = countInspection(schedule);
+				countInspection.add(inspection);
+				suminspect = suminspect +inspection;
+				sumcleaning = sumcleaning +cleaning;
+				sumwashing = sumwashing + washing;
+				sumrepair = sumrepair+repair;
+			}
+			double avgcleaning=sumcleaning/numberOfSeeds;
+			double avgwashing=sumwashing/numberOfSeeds;
+			double avgrepair=sumrepair/numberOfSeeds;
+			double avginspect=suminspect/numberOfSeeds;
+
+			averageNumberNeedCleaning.add(avgcleaning);
+			averageNumberNeedInspection.add(avginspect);
+			averageNumberNeedWashing.add(avgwashing);
+			averageNumberNeedRepair.add(avgrepair);
 		}
+		System.out.println("Average number need inspection "+averageNumberNeedInspection.toString());
+		System.out.println("Average number need cleaning "+ averageNumberNeedCleaning.toString());
+		System.out.println("Average number need washing "+averageNumberNeedWashing.toString());
+		System.out.println("Average number need repair "+averageNumberNeedRepair.toString());
+
+
+
+
+
+
+	}
 
 
 
